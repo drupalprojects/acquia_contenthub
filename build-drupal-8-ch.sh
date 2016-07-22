9a866ffb312510c5be13836c092c3b3d7de9ddd7
Matthew Morley <matthew.morley@acquia.com>
2016-07-22 11:20:20 -0400

    CHMS-814 | streamline D8 build script

--- build-drupal-8-ch.sh
+++ build-drupal-8-ch.sh
@@ -7,15 +7,24 @@ set -e
 if [ -z "$1" ]; then
   echo "You need to specify a directory name to build Drupal 8."
   echo "You can also specify an optional branch."
-  echo "ie. ./build-drupal-8-ch.sh d8 CHMS-779"
+  echo "ie. ./build-drupal-8-ch.sh d8 CHMS-814"
   exit
 fi
 
 # project creation
 if [ ! -d $1 ]; then
-  composer create-project drupal-composer/drupal-project:~8.0 $1 --stability dev --no-interaction --profile
+  # dont install yet, just create
+  composer create-project drupal-composer/drupal-project:~8.0 $1 --stability dev --no-interaction --no-install --prefer-dist --profile
+
+  # ugh, see https://github.com/drupal-composer/drupal-project/issues/175
   cd $1
+  echo ">adjusting config settings"
+  composer config --global repo.packagist composer https://packagist.org
+  composer config --global repositories.0 composer https://packages.drupal.org/8
   composer config repositories.acquia-content-hub-d8 vcs https://github.com/acquia/content-hub-d8
+
+  echo ">composer update"
+  composer update --profile
 else
   echo " using existing project $1..."
   cd $1
@@ -29,12 +38,13 @@ fi
 
 # setup the content-hub-d8 module and change branch if needed
 if [ ! -d web/modules/contrib/content-hub-d8 ]; then
-  echo " performing initial content-hub-d8 require"
+  echo ">composer update for content-hub-d8"
   composer require acquia/content-hub-d8:dev-develop --profile
+  echo ">removeing composer remote"
   cd web/modules/contrib/content-hub-d8
   git remote rm composer
 else
-  echo " using existing content-hub-d8 require"
+  echo " using existing content-hub-d8"
   cd web/modules/contrib/content-hub-d8
   git fetch
   echo " checking out $branch"
