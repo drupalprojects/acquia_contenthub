d155de65c00a8df8698e378a0563dc1dc5251d5d
scor <scorlosquet@gmail.com>
2016-07-29 11:26:48 -0400

    CHMS-814 | use second argument in wrapper script

--- scripts/create-drupal-8-ch.sh
+++ scripts/create-drupal-8-ch.sh
@@ -2,4 +2,4 @@
 
 # Wrapper script combining a build followed by a Drupal site install.
 
-./build-drupal-8-ch.sh $1 && ./install-drupal-8-ch.sh $1
+./build-drupal-8-ch.sh $1 $2 && ./install-drupal-8-ch.sh $1
