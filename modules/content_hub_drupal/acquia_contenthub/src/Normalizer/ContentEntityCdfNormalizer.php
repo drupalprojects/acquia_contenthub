40c5ef6e4060f8dba51c086d9afbf20aa20d747f
Alejandro Barrios <abarrios@ieee.org>
2016-08-25 12:55:36 -0400

    CHMS-770 | More hacks for vocabularies

--- modules/content_hub_drupal/acquia_contenthub/src/Normalizer/ContentEntityCdfNormalizer.php
+++ modules/content_hub_drupal/acquia_contenthub/src/Normalizer/ContentEntityCdfNormalizer.php
@@ -554,8 +554,11 @@ class ContentEntityCdfNormalizer extends NormalizerBase {
     $ref_entities = $this->getReferencedFields($entity, $context);
     foreach ($ref_entities as $key => $entity) {
       if (!in_array($entity->uuid(), $uuids)) {
-        $referenced_entities[] = $entity;
-        $this->getMultilevelReferencedFields($entity, $referenced_entities, $context);
+        // @TODO: This if-condition is a hack to avoid Vocabulary entities.
+        if (in_array('Drupal\Core\Entity\ContentEntityInterface', class_implements($entity))) {
+          $referenced_entities[] = $entity;
+          $this->getMultilevelReferencedFields($entity, $referenced_entities, $context);
+        }
       }
     }
 
