23f399ff431fba3c2a5abf29d41de4709132980a
Charu Agarwal <charu.ag6@gmail.com>
2016-10-25 08:30:32 +0530

    CHMS-958 | Prefill the webhook url

--- modules/content_hub_drupal/acquia_contenthub/src/Form/WebhooksSettingsForm.php
+++ modules/content_hub_drupal/acquia_contenthub/src/Form/WebhooksSettingsForm.php
@@ -84,8 +84,8 @@ class WebhooksSettingsForm extends ConfigFormBase {
 
     $webhook_url = Url::fromUri('internal:/acquia-contenthub/webhook', array(
       'absolute' => TRUE,
-    ));
-    $webhook_url = $config->get('webhook_url');
+    ))->toString();
+    $webhook_url = NULL !== $config->get('webhook_url') ? $config->get('webhook_url') : $webhook_url;
     $webhook_uuid = $config->get('webhook_uuid');
     $readonly = (bool) $webhook_uuid ? ['readonly' => TRUE] : [];
 
@@ -102,7 +102,7 @@ class WebhooksSettingsForm extends ConfigFormBase {
       '#type' => 'textfield',
       '#title' => t('Acquia Content Hub URL'),
       '#description' => t('Please use a full URL (Ex. http://example.com/webhook). This is the end-point where this site will receive webhooks from Acquia Content Hub.'),
-      '#default_value' => $config->get('webhook_url'),
+      '#default_value' => $webhook_url,
       '#required' => TRUE,
     );
 
