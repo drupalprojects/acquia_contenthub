a484a06e0ec2c57c53f89acf4a3cac981b327bb6
Alejandro Barrios <abarrios@ieee.org>
2016-10-27 16:24:41 -0400

    CHMS-972 | isEligibleEntity() should also read from configuration entity.

--- modules/content_hub_drupal/acquia_contenthub/src/EntityManager.php
+++ modules/content_hub_drupal/acquia_contenthub/src/EntityManager.php
@@ -417,7 +417,11 @@ class EntityManager {
    *   to content hub.
    */
   public function isEligibleEntity(EntityInterface $entity) {
-    $entity_type_config = $this->configFactory->get('acquia_contenthub.entity_config')->get('entities.' . $entity->getEntityTypeId());
+    /** @var \Drupal\rest\RestResourceConfigInterface $contenthub_entity_config_storage */
+    $contenthub_entity_config_storage = $this->entityTypeManager->getStorage('acquia_contenthub_entity_config');
+    $contenthub_entity_configs = $contenthub_entity_config_storage->loadMultiple(array($entity->getEntityTypeId()));
+    $entity_type_config = isset($contenthub_entity_configs[$entity->getEntityTypeId()]) ? $contenthub_entity_configs[$entity->getEntityTypeId()]->getBundles() : NULL;
+
     $bundle_id = $entity->bundle();
     if (empty($entity_type_config) || empty($entity_type_config[$bundle_id]) || empty($entity_type_config[$bundle_id]['enable_index'])) {
       return FALSE;
