9276af6fff8648bac28898e0ff85c7d0e1f2d831
Alejandro Barrios <abarrios@ieee.org>
2016-11-28 11:55:59 -0500

    CHMS-1030 | Convert integer values to boolean

--- modules/content_hub_drupal/acquia_contenthub/acquia_contenthub.post_update.php
+++ modules/content_hub_drupal/acquia_contenthub/acquia_contenthub.post_update.php
@@ -21,6 +21,12 @@ function acquia_contenthub_post_update_create_acquia_contenthub_entity_config_en
   /** @var \Drupal\Core\Config\Entity\ConfigEntityInterface $contenthub_entity_config_old */
   $contenthub_entity_config_old = \Drupal::state()->get('acquia_contenthub_update_8201_entity_config', []);
   foreach ($contenthub_entity_config_old as $entity_type => $bundles) {
+    // Convert integer value to boolean.
+    foreach ($bundles as $type => $bundle) {
+      $bundles[$type]['enable_index'] = boolval($bundle['enable_index']);
+      $bundles[$type]['enable_viewmodes'] = boolval($bundle['enable_viewmodes']);
+     }
+    // Saving configuration entities.
     $contenthub_entity_config = ContentHubEntityTypeConfig::create([
       'id' => $entity_type,
       'bundles' => $bundles,
