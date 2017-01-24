49328a0a69dfddc508d25a6f438b99d251dd4eb3
Yuan Xie <yuan.xie@acquia.com>
2017-01-24 17:48:19 -0500

    CHMS-1171 | Add functional test for the 'set to locally-changed flag' feature.

--- modules/content_hub_drupal/acquia_contenthub/src/Tests/NodeFormTest.php
+++ modules/content_hub_drupal/acquia_contenthub/src/Tests/NodeFormTest.php
@@ -24,7 +24,7 @@ class NodeFormTest extends WebTestBase {
   private $article;
 
   /**
-   * Configure and use content hub preview image style.
+   * Configure content hub node form.
    */
   public function testNodeForm() {
     $this->drupalLogin($this->adminUser);
@@ -49,8 +49,44 @@ class NodeFormTest extends WebTestBase {
     $edit['acquia_contenthub[auto_update]'] = FALSE;
     $this->drupalPostForm($node_edit_url, $edit, t('Save'));
 
-    // Form should have option, and default to "enabled".
+    // Option should now set to to "disabled".
     $this->drupalGet($node_edit_url);
+    $this->assertNoFieldChecked('edit-acquia-contenthub-auto-update', 'Automatic updates is enabled by default');
+  }
+
+  /**
+   * Configure content hub node form, auto setting to "having local change".
+   */
+  public function testNodeFormIntroduceLocalChange() {
+    $this->drupalLogin($this->adminUser);
+    $this->article = $this->drupalCreateNode(array('type' => 'article'));
+
+    // Convert the node into a Content Hub node.
+    $this->convertToContentHubEntity($this->article);
+
+    // A Content Hub node should have Acquia Content Hub settings.
+    // Form should have option, and default to "enabled".
+    $node_edit_url = 'node/' . $this->article->id() . '/edit';
+    $this->drupalGet($node_edit_url);
+    $this->assertNoText(t('This content has been modified.'));
+    $this->assertFieldChecked('edit-acquia-contenthub-auto-update', 'Automatic updates is enabled by default');
+
+    // Don't edit anything, save.
+    $this->drupalPostForm($node_edit_url, [], t('Save'));
+
+    // Option should still be set to "enabled".
+    $this->drupalGet($node_edit_url);
+    $this->assertNoText(t('This content has been modified.'));
+    $this->assertFieldChecked('edit-acquia-contenthub-auto-update', 'Automatic updates is enabled by default');
+
+    // Edit title.
+    $edit = [];
+    $edit['title[0][value]'] = 'my new title';
+    $this->drupalPostForm($node_edit_url, $edit, t('Save'));
+
+    // Option should now set to to "disabled, as having local changes".
+    $this->drupalGet($node_edit_url);
+    $this->assertText(t('This content has been modified.'));
     $this->assertNoFieldChecked('edit-acquia-contenthub-auto-update', 'Automatic updates is enabled by default');
   }
 
