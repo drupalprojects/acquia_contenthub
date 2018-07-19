b868a2e3cd282bb95d78888b0428b783fc06b91f
Stephane Corlosquet <scorlosquet@gmail.com>
2018-07-19 16:23:14 -0400

    LCH-2036: Prevent import and export queues from running during Drupal cron (#255)

--- src/Plugin/QueueWorker/ContentHubExportQueue.php
+++ src/Plugin/QueueWorker/ContentHubExportQueue.php
@@ -3,12 +3,11 @@
 namespace Drupal\acquia_contenthub\Plugin\QueueWorker;
 
 /**
- * Export content to Content Hub service.
+ * Export content to the Acquia Content Hub service.
  *
  * @QueueWorker(
  *   id = "acquia_contenthub_export_queue",
- *   title = @Translation("Export Content to Acquia Content Hub"),
- *   cron = {"time" = 60}
+ *   title = @Translation("Export Content to Acquia Content Hub")
  * )
  */
 class ContentHubExportQueue extends ContentHubExportQueueBase {}
